d483caf7c079d3258dd0f18ecb1dda1af2b0b339
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-02-18 19:23:37 +0100

    Update song.js

--- src/classes/scraper/song.js
+++ src/classes/scraper/song.js
@@ -2,10 +2,14 @@ class ScrapedSong{
     constructor(data){
         this.name = data.name;
         this.artist = data.description.split(`Listen to ${this.name} on Spotify. `)[1].split(' ·')[0];
+        this.artistUrl = `https://open.spotify.com/artist/${data.artistId}`;
+        this.artistId = data.artistId;
         this.description = data.description;
         this.publishDate = data.datePublished;
         this.locations = data.potentialAction.expectsAcceptanceOf.eligibleRegion.map(t => t.name);
         this.url = data.url;
+        this.embedUrl = `https://open.spotify.com/embed/track/${data.id}`;
+        this.id = data.id;
     }
 }
 
