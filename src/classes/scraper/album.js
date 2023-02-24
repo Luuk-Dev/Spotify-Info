bd5e8fc79dde59ad9ee4355b05ec2fbee78edf03
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-02-24 22:30:31 +0100

    Update album.js

--- src/classes/scraper/album.js
+++ src/classes/scraper/album.js
@@ -5,8 +5,15 @@ class ScrapedAlbum{
         this.id = data.albumId;
         this.name = data.albumName;
         this.artist = data.artist;
+        this.artistId = data.artistId ? data.artistId : null;
+        this.artistUrl = data.artistId ? `https://open.spotify.com/artist/${data.artistId}` : null;
         this.url = `https://open.spotify.com/album/${data.albumId}`;
         this.embedUrl = `https://open.spotify.com/embed/album/${data.albumId}`;
+        this.publishDate = data.publishDate || null;
+        this.locations = data.locations || [];
+        this.description = data.description || null;
+
+        this.thumbnail = data.thumbnail ? data.thumbnail : null;
 
         this.songs = [];
 
