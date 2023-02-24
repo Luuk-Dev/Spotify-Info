1e40f7d41697b2fd92821ae3824d066a4b5b187a
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-02-24 22:30:45 +0100

    Update playlist.js

--- src/classes/scraper/playlist.js
+++ src/classes/scraper/playlist.js
@@ -5,8 +5,14 @@ class ScrapedPlaylist{
         this.id = data.playlistId;
         this.name = data.playlistName;
         this.creator = data.playlistCreator;
+        this.creatorId = data.playlistCreatorId ? data.playlistCreatorId : null;
+        this.creatorUrl = data.playlistCreatorId ? `https://open.spotify.com/user/${data.playlistCreatorId}` : null;
         this.url = `https://open.spotify.com/playlist/${data.playlistId}`;
         this.embedUrl = `https://open.spotify.com/embed/playlist/${data.playlistId}`;
+        this.locations = data.locations || [];
+        this.description = data.description || null;
+
+        this.thumbnail = data.thumbnail ? data.thumbnail : null;
 
         this.songs = [];
 
