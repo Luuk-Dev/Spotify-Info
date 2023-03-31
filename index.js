29f42bd6941a540ece168c84745e5c32bc7b1c07
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-03-31 20:22:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const validateFunctions = require('./src/validate.js');
 const scrapeFunctions = require('./src/scraper.js');
+const { setApiCredentials } = require('./src/functions.js');
+const getTrack = require('./src/api/getTrack.js');
+const getAlbum = require('./src/api/getAlbum.js');
+const getPlaylist = require('./src/api/getPlaylist.js');
+const getArtist = require('./src/api/getArtist.js');
+const search = require('./src/api/search.js');
 
-module.exports = {...validateFunctions, ...scrapeFunctions};
+module.exports = {...validateFunctions, ...scrapeFunctions, setApiCredentials, getTrack, getAlbum, getPlaylist, getArtist, search};
