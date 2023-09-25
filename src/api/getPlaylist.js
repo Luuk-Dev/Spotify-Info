7ae04029b2068f9e2cb29b7a2de579f6485c0053
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-09-25 09:11:00 +0200

    Update getPlaylist.js

--- src/api/getPlaylist.js
+++ src/api/getPlaylist.js
@@ -1,5 +1,5 @@
 const { request } = require('../request/index.js');
-const { getCredentials, getAccessToken, getApiEndpoint } = require('../functions.js');
+const { getCredentials, getAccessToken, getApiEndpoint, wait } = require('../functions.js');
 const { validateID, getID, validatePlaylistURL } = require('../validate.js');
 const { URL } = require('url');
 const ApiPlaylist = require('../classes/api/playlist.js');
@@ -21,9 +21,17 @@ function getPlaylist(url, options){
         if(!playlistId) return reject('Invalid url: Cannot get ID of playlist url');
 
         let searchParams = "";
+        let searchParamsTracks = [];
         if(typeof options === 'object' && !Array.isArray(options) && options !== null){
             if(typeof options.market === 'string'){
                 searchParams += "?market="+options.market;
+            }
+            if(['string', 'number'].indexOf(typeof options.offset) >= 0){
+                searchParamsTracks.push(`offset=${options.offset}`);
+            }
+            if(['string', 'number'].indexOf(typeof options.limit) >= 0){
+                if(parseInt(options.limit) < 0 || parseInt(options.limit) > 100) return reject(`The range of the limit of the tracks is 0-100`);
+                searchParamsTracks.push(`limit=${options.limit}`);
             }
         }
 
@@ -35,16 +43,35 @@ function getPlaylist(url, options){
                 'Authorization': accessToken.type+' '+accessToken.token
             },
             method: 'GET'
-        }).then(res => {
-            let response = res;
-            if(typeof response !== 'object'){
-                try{
-                    response = JSON.parse(response);
-                } catch (err){
-                    return reject('Cannot transform response \''+response+'\' to JSON format');
+        }).then(async res => {
+                await wait(400);
+                let response = res;
+                if(typeof response !== 'object'){
+                    try{
+                        response = JSON.parse(response);
+                    } catch (err){
+                        return reject('Cannot transform response \''+response+'\' to JSON format');
+                    }
                 }
-            }
-            resolve(new ApiPlaylist(response));
+                const tracksEndpoint = getApiEndpoint()+"playlists/"+playlistId+"/tracks"+(searchParamsTracks.length > 0 ? `?${searchParamsTracks.join('&')}` : ``);
+                const parsedTracksEndpoint = new URL(tracksEndpoint);
+
+                request(parsedTracksEndpoint, {
+                    headers: {
+                        'Authorization': accessToken.type+' '+accessToken.token
+                    },
+                    method: 'GET'
+                }).then(songRes => {
+                    let songResponse = songRes;
+                    if(typeof songResponse !== 'object'){
+                        try{
+                            songResponse = JSON.parse(songResponse);
+                        } catch (err){
+                            return reject('Cannot transform response \''+songResponse+'\' to JSON format');
+                        }
+                    }
+                    resolve(new ApiPlaylist(response, songResponse));
+                }).catch(reject);
         }).catch(reject);
     });
 }
