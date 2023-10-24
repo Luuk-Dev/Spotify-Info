4a6b2eb62df34f08c2e89d0b80682ed40a6d879e
Luuk <70959239+Luuk-Dev@users.noreply.github.com>
2023-10-24 15:05:01 +0200

    Update validate.js

--- src/validate.js
+++ src/validate.js
@@ -30,7 +30,7 @@ function validateTrackURL(url){
 
     const parsed = validateURL(url);
 
-    let reg = new RegExp('/(.+)/(track|embed/track|v1/tracks)/([a-zA-Z0-9]{10,30})');
+    let reg = new RegExp('((.+)|)/(track|embed/track|v1/tracks)/([a-zA-Z0-9]{10,30})');
     if(!reg.test(parsed.pathname)) return false;
 
     const trackId = (parsed.pathname.split('/track/')[1] || parsed.pathname.split('/v1/tracks/')[1] || '').split('/')[0];
@@ -44,7 +44,7 @@ function validateArtistURL(url){
     
     const parsed = validateURL(url);
 
-    let reg = new RegExp('/(.+)/(artist|embed/artist|v1/artists)/([a-zA-Z0-9]{10,30})');
+    let reg = new RegExp('((.+)|)/(artist|embed/artist|v1/artists)/([a-zA-Z0-9]{10,30})');
     if(!reg.test(parsed.pathname)) return false;
 
     const artistId = (parsed.pathname.split('/artist/')[1] || parsed.pathname.split('/v1/artists/')[1] || '').split('/')[0];
@@ -58,7 +58,7 @@ function validateAlbumURL(url){
     
     const parsed = validateURL(url);
 
-    let reg = new RegExp('/(.+)/(album|embed/album|v1/albums)/([a-zA-Z0-9]{10,30})');
+    let reg = new RegExp('((.+)|)/(album|embed/album|v1/albums)/([a-zA-Z0-9]{10,30})');
     if(!reg.test(parsed.pathname)) return false;
 
     const albumId = (parsed.pathname.split('/album/')[1] || parsed.pathname.split('/v1/albums/')[1] || '').split('/')[0];
@@ -72,7 +72,7 @@ function validatePlaylistURL(url){
     
     const parsed = validateURL(url);
 
-    let reg = new RegExp('/(.+)/(playlist|embed/playlist|v1/playlists)/([a-zA-Z0-9]{10,30})');
+    let reg = new RegExp('((.+)|)/(playlist|embed/playlist|v1/playlists)/([a-zA-Z0-9]{10,30})');
     if(!reg.test(parsed.pathname)) return false;
 
     const playlistId = (parsed.pathname.split('/playlist/')[1] || parsed.pathname.split('/v1/playlists/')[1] || '').split('/')[0];
@@ -86,7 +86,7 @@ function validateUserURL(url){
     
     const parsed = validateURL(url);
 
-    let reg = new RegExp('/(.+)/(user|v1/users)/([a-zA-Z0-9]{10,30})');
+    let reg = new RegExp('((.+)|)/(user|v1/users)/([a-zA-Z0-9]{10,30})');
     if(!reg.test(parsed.pathname)) return false;
 
     const userId = (parsed.pathname.split('/user/')[1] || parsed.pathname.split('/v1/users/')[1] || '').split('/')[0];
